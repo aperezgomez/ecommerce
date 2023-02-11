5593d8e35881cf4e85def61af132de70901521a0
becomealpha <120771676+becomealpha@users.noreply.github.com>
2023-02-11 09:34:18 -0500

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,8 @@ import ItemDetails from './scenes/itemDetails/ItemDetails';
 import Checkout from './scenes/checkout/Checkout';
 import Confirmation from './scenes/checkout/Confirmation';
 import Navbar from './scenes/global/Navbar';
-import CartMenu from './scenes/global/CartMenu'
+import CartMenu from './scenes/global/CartMenu';
+import Footer from './scenes/global/Footer';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -35,6 +36,7 @@ function App() {
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
         <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div>
   );
